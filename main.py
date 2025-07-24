bcb18085b749940e834105057d7227b5e6d96ec1
Marcello Feroce <ferocemarcello@gmail.com>
2025-07-24 21:36:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
-def iterate_(board):
-    
-    return new_board
+import copy
 
+def iterate_(board):
+    new_board = copy.deepcopy(board)
+
+    num_rows = len(board)
+    if num_rows == 0:
+        return new_board
+    num_cols = len(board[0])
+    col_index = 0
+    # Loop for every column (from 0 to the last one)
+    while col_index in < num_cols:
+        # Step 1: Check if the current column contains '#'
+        column_contains_hash = False
+        for row_index_check in range(num_rows):
+            if board[row_index_check][col_index] == '#':
+                column_contains_hash = True
+                break # Found '#', no need to check further in this column
+
+        # If the column does not contain '#', move to the next column
+        if column_contains_hash:
+            row_index = num_rows -1
+            while row_index >= 0:
+                if new_board[row_index][col_index] == '#':
+                    if row_index == num_rows -1:
+                        pass
+                    else:
+                        pass
+                if new_board[row_index][col_index] == '-':
+                    pass
+                if new_board[row_index][col_index] == '*':
+                    pass
+        col_index+=1
+
+    return new_board
+    
 def solution(board):
     current_board = board
     while True:
